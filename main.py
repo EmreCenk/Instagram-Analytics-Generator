a3c898d5927fdfcf1e5c5752d24ba03778cc5cf3
Emre Cenk <emrecenk9@gmail.com>
2022-01-10 16:58:25 -0500

    created access for advertiser company information

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from contextlib import contextmanager
 import os
+from contextlib import contextmanager
+from typing import List, Dict
+import json
 
 @contextmanager
 def different_cwd(path: str):
@@ -13,8 +15,30 @@ def different_cwd(path: str):
     finally:
         os.chdir(oldpwd)
 
-path_to_data = r""
-with different_cwd(path_to_data):
-    # Everything inside this block will be in the new working directory
-    # Once this block is exited, the working directory will be reset to the initial
-    print(os.listdir())
+class InstagramDataAnalyzer():
+    @staticmethod
+    def get_marketing_list(path: str) -> List[Dict]:
+        """
+        :param path: Path to root folder of downloaded export
+        :return: list of company dictionaries.
+        The entries are in the following format:
+        {'advertiser_name': 'name',
+         'has_data_file_custom_audience': True,
+          'has_remarketing_custom_audience': True,
+           'has_in_person_store_visit': False}
+        """
+        # Finding path to ads from root:
+        path_to_ads = os.path.join(path, "ads_and_businesses")
+        path_to_ads = os.path.join(path_to_ads, os.listdir(path_to_ads)[0])
+
+        with open(path_to_ads) as ad_file:
+            data = json.load(ad_file)["ig_custom_audiences_all_types"]
+        print(data)
+        return data
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv()
+
+
+    path_to_data = os.environ["path_to_instagram_export_download"]
+    InstagramDataAnalyzer.get_marketing_list(path_to_data)
