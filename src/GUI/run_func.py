60eaa514c7bb9ecefde75a521f61d3fae7291c4a
Emre Cenk <emrecenk9@gmail.com>
2022-01-22 00:11:44 -0500

    autofill implemented

--- src/GUI/run_func.py
+++ src/GUI/run_func.py
@@ -7,9 +7,12 @@ from src.Handling_Data import utils
 
 class TrueFalseComboBox(ttk.Combobox):
     def __init__(self, *args):
-        super().__init__(*args, values = ["True", "False"])
+        super().__init__(*args, values = ["True", "False"], state = "readonly")
 
-def run_func(func_to_run: Callable) -> None:
+def run_func(func_to_run: Callable, ready_inputs=None) -> None:
+
+    if ready_inputs is None: ready_inputs = {}
+
     window = tk.Tk()
     window.title("Welcome to TutorialsPoint")
     window.geometry('400x400')
@@ -30,7 +33,11 @@ def run_func(func_to_run: Callable) -> None:
         type_of_widget = {int: tk.Entry,
                           bool: TrueFalseComboBox,
                           str: tk.Entry}[types[i]]
-        entries.append(type_of_widget(window).place(relx=width, rely=offset*i, relwidth=1-width, relheight=offset, anchor="nw"))
+        current = type_of_widget(window)
+        current.place(relx=width, rely=offset*i, relwidth=1-width, relheight=offset, anchor="nw")
+        if params[i] in ready_inputs: current.insert(0, ready_inputs[params[i]])
+
+        entries.append(current)
     try: i
     except: return
     tk.Button(text = "Generate Graph").place(relx=0.5, rely=offset*(i+1), relwidth=0.4, relheight=offset, anchor="n")
@@ -38,7 +45,7 @@ def run_func(func_to_run: Callable) -> None:
 
 if __name__ == '__main__':
     from src.Handling_Data.Visualizing_Data import InstagramDataVisualizer
-    run_func(InstagramDataVisualizer.visualize_most_active_year)
+    run_func(InstagramDataVisualizer.visualize_most_active_year, ready_inputs = {"path": "somepath"})
 
 
 
