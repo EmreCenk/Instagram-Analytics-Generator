1d4773c2bfe6cffff23274f11d18a3ef854c0185
Emre Cenk <emrecenk9@gmail.com>
2022-01-22 00:00:28 -0500

    implemented dynamic inputs for arg types in form

--- src/GUI/run_func.py
+++ src/GUI/run_func.py
@@ -1,23 +1,48 @@
 
 import tkinter as tk
+from tkinter import ttk
 from typing import Callable
+import inspect
+from src.Handling_Data import utils
+
+class TrueFalseComboBox(ttk.Combobox):
+    def __init__(self, *args):
+        super().__init__(*args, values = ["True", "False"])
+
 def run_func(func_to_run: Callable) -> None:
     window = tk.Tk()
     window.title("Welcome to TutorialsPoint")
     window.geometry('400x400')
     window.configure(background="grey")
 
-    params = ["alpha", "beta", "theta"]
+    params = []
+    types = []
+    things = inspect.signature(func_to_run)
+    for arg in things.parameters:
+        types.append(things.parameters[arg].annotation)
+        params.append(arg)
     offset = 0.1
     width = 0.5
     labels = []
     entries = []
     for i in range(len(params)):
         labels.append(tk.Label(window, text=params[i]).place(relx=0, rely=offset*i, relwidth=width, relheight=offset, anchor="nw"))
-        entries.append(tk.Entry(window).place(relx=width, rely=offset*i, relwidth=1-width, relheight=offset, anchor="nw"))
-
+        type_of_widget = {int: tk.Entry,
+                          bool: TrueFalseComboBox,
+                          str: tk.Entry}[types[i]]
+        entries.append(type_of_widget(window).place(relx=width, rely=offset*i, relwidth=1-width, relheight=offset, anchor="nw"))
+    try: i
+    except: return
     tk.Button(text = "Generate Graph").place(relx=0.5, rely=offset*(i+1), relwidth=0.4, relheight=offset, anchor="n")
     window.mainloop()
 
 if __name__ == '__main__':
-    run_func("")
+    from src.Handling_Data.Visualizing_Data import InstagramDataVisualizer
+    run_func(InstagramDataVisualizer.visualize_most_active_year)
+
+
+
+
+
+
+
