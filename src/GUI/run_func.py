ec9fe61d314b9dcbcdbf18879e85b9cb7237bce4
Emre Cenk <emrecenk9@gmail.com>
2025-03-31 04:55:31 -0400

    added Literal type compatability to dynamically generated UI

--- src/GUI/run_func.py
+++ src/GUI/run_func.py
@@ -1,7 +1,7 @@
 
 import tkinter as tk
 from tkinter import ttk
-from typing import Callable
+from typing import *
 import inspect
 from src.Handling_Data.Retreiving_Data import InstagramDataRetreiver
 class TrueFalseComboBox(ttk.Combobox):
@@ -15,6 +15,10 @@ class ChatNameComboBox(ttk.Combobox):
     def __init__(self, *args, path: str):
         super().__init__(*args, values = InstagramDataRetreiver.list_chats(path), state = "readonly")
 
+class LiteralComboBox(ttk.Combobox):
+    def __init__(self, *args, options=[]):
+        super().__init__(*args, values=options, state = "readonly")
+        
 def run_func_via_gui(func_to_run: Callable, ready_inputs=None) -> None:
 
     if ready_inputs is None: ready_inputs = {}
@@ -33,14 +37,18 @@ def run_func_via_gui(func_to_run: Callable, ready_inputs=None) -> None:
         real_args = []
         for i in range(len(args)):
             if types[i] == bool: real_args.append(args[i] == "True")
+            elif hasattr(types[i], "__origin__") and types[i].__origin__ is Literal: # Using Litera["a", "b"] as an input type for more descriptiveness
+                print("ya")
+                real_args.append(args[i])
             else:
                 try: real_args.append(types[i](args[i]))
                 except ValueError: warning["text"] = f"Please make sure {params[i]} is an integer.";warning.update(); return
         print(func_to_run, real_args)
         func_to_run(*real_args)
+        
     window = tk.Tk()
     window.title("Tweak Options For Graph")
-    window.geometry('400x400')
+    window.geometry('650x400')
     window.configure(background="grey")
 
     params = []
@@ -49,22 +57,28 @@ def run_func_via_gui(func_to_run: Callable, ready_inputs=None) -> None:
     for arg in things.parameters:
         types.append(things.parameters[arg].annotation)
         params.append(arg)
-
+    print(types)
+    print(params)
     offset = 0.1
     width = 0.5
     labels = []
     entries = []
     type_to_widg = {int: tk.Entry,
                     bool: TrueFalseComboBox,
-                    str: tk.Entry}
+                    str: tk.Entry,
+                    }
 
     for i in range(len(params)):
         labels.append(tk.Label(window, text=params[i]).place(relx=0, rely=offset*i, relwidth=width, relheight=offset, anchor="nw"))
-        type_of_widget = type_to_widg[types[i]]
 
-        if params[i] == "interval": current = IntervalComboBox(window)
+        if hasattr(types[i], "__origin__") and types[i].__origin__ is Literal: # Using Litera["a", "b"] as an input type for more descriptiveness
+            literal_values = set(types[i].__args__)
+            current = LiteralComboBox(window, options=sorted(literal_values, key=lambda x: -len(x)))
+        elif params[i] == "interval": current = IntervalComboBox(window)
         elif params[i] == "chat_name" and "path" in ready_inputs: current = ChatNameComboBox(window, path = ready_inputs["path"])
-        else: current = type_of_widget(window)
+        else: 
+            type_of_widget = type_to_widg[types[i]]
+            current = type_of_widget(window)
 
         current.place(relx=width, rely=offset*i, relwidth=1-width, relheight=offset, anchor="nw")
         if params[i] in ready_inputs:
@@ -74,7 +88,9 @@ def run_func_via_gui(func_to_run: Callable, ready_inputs=None) -> None:
         entries.append(current)
     try: i
     except: return
+
     tk.Button(window, text = "Generate Graph", command = execute).place(relx=0.5, rely=offset*(i+1), relwidth=0.4, relheight=offset, anchor="n")
+
     warning = tk.Label(window, text = "")
     warning.place(relx=0.5, rely=offset * (i + 2), relwidth=1,
                                                              relheight=offset, anchor="n")
